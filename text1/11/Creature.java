350038252c40fda1af391d8d0f0774ac34639aa0
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-06-29 22:19:39 +0900

    新しいJavaプログラムの例を追加しました。Character、Dancer、Hero、Matango、CleaningService、KyotoCleaningShop、Asset、TangibleAsset、Book、Computer、Thingインターフェースおよびクラスを含み、それぞれの属性や行動を定義しています。また、メインクラスでの動作を確認できるようにしました。

---
+++ text1/11/Creature.java
@@ -0,0 +1,4 @@
+public interface Creature {
+    // public abstract void run()と同じ
+    void run();
+}
---
+++ text1/11/Hero.java
@@ -0,0 +1,26 @@
+public class Hero {
+    //属性の定義
+    String name = "湊";
+    int hp;
+
+    //操作の定義
+    public void attack(Matango m){
+        System.out.println(this.name + "の攻撃!");
+        m.hp -= 5;
+        System.out.println("5ポイントのダメージを与えた");
+    }
+
+    public void run(){
+        System.out.println(this.name + "は逃げ出した！");
+    }
+
+    public final void slip(){
+        this.hp -= 5;
+        System.out.println(this.name + "は転んだ！");
+        System.out.println("5のダメージ");
+    }
+
+    public Hero(){
+        System.out.println("Heroのコンストラクタが動作");
+    }
+}
