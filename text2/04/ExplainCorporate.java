6e7f5f0fd07458e4c7a8753f91944d3c21274a51
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-07-13 02:01:34 +0900

    新しいJavaプログラムの例を追加しました。Ask、ExplainCorporate、Greetクラスを実装し、ユーザーからの入力を受け取る機能や企業情報の表示を行います。

---
+++ text2/04/ExplainCorporate.java
@@ -0,0 +1,6 @@
+public class ExplainCorporate {
+    public static void main(String[] rags){
+        System.out.println("---は1997年に創業されました");
+        System.out.println("コーポレートスローガンは\"Hello World, Hello innovation\"です。");
+    }
+}
