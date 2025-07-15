e42005d83f9d5627b5387fc5ecdd193d04ac67db
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-07-16 01:17:31 +0900

    CalcBmiクラスのBMI計算ロジックを改善し、健康状態を4つのカテゴリ（瘦せすぎ、標準、太り気味、太りすぎ）で表示する機能を追加しました。

--- text2/06/CalcBmi.java
+++ text2/06/CalcBmi.java
@@ -3,8 +3,16 @@ public class CalcBmi {
         double height = Double.parseDouble(args[0]);
         double weight = Double.parseDouble(args[1]);
         double bmi = weight/(height*height);
-
-        System.out.printf("あなたの健康状態: 標準 (BMI=%.5f)", bmi);
+        String[] words = {"瘦せすぎ","標準","太り気味","太りすぎ"};
+        if (bmi < 18.5){
+            System.out.printf("あなたの健康状態: %s (BMI=%.5f)", words[0], bmi);
+        }else if(bmi < 25){
+            System.out.printf("あなたの健康状態: %s (BMI=%.5f)", words[1], bmi);
+        }else if(bmi < 30){
+            System.out.printf("あなたの健康状態: %s (BMI=%.5f)", words[2], bmi);
+        }else{
+            System.out.printf("あなたの健康状態: %s (BMI=%.5f)", words[3], bmi);
+        }
 
     }
 }
